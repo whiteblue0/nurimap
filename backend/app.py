cba2bc0654a434fdd280ca829e1f2c68cab29fa1
whiteblue <21port@naver.com>
2020-08-02 21:44:42 +0900

    add flask cors

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, reqparse, Api
-
+from flask_cors import CORS, cross_origin
 class marketData(Resource):
     def get(self):
         result = 'result'
@@ -8,6 +8,7 @@ class marketData(Resource):
 
 
 app = Flask("NuriMap")
+CORS(app,resources={"/markets": {"origins" : "*"}})
 api = Api(app)
 api.add_resource(marketData, '/markets')
 
